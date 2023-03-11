935f52079975f302f52a886e0549ea65f46d57c8
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-03-11 19:15:51 -0300

    Initial commit

---
+++ src/lib/navBar.svelte
@@ -0,0 +1,36 @@
+<script lang='ts'>
+    import magnify from '$lib/assets/magnify.svg';
+</script>
+<nav>
+    <div id='search-container'>
+        <form action="">
+            <button><img src={magnify} alt="Magnify img" srcset=""></button>
+            <input type="text" name='search' placeholder="Type something...">
+        </form>
+    </div>
+    <div id='notiUser'>
+
+    </div>
+    <div id='btns'>
+        <button type="button">
+            NEW
+        </button>
+        <button type="button">
+            UPLOAD
+        </button>
+        <button type="button">
+            SHARE
+        </button>
+    </div>
+</nav>
+
+
+<style lang='scss'>
+    div#search-container{
+        button{
+            width: 30px;
+            border: 0;
+            border-radius: 10px;
+        }
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import NavBar from "../lib/navBar.svelte";
+</script>
+
+<NavBar></NavBar>
+<slot></slot>
+
+
+
+
+
+
+<style>
+
+</style>
